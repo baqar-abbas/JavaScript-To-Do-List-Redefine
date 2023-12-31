1dee318c36fae763b8e324215d55756004335391
baqar-abbas <baqar007@hotmail.com>
2023-12-31 05:09:16 +0500

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-/* eslint-disable */
 import './index.css';
 import select2 from '../images/select.png';
 import delete2 from '../images/delete.png';
 import refresh from '../images/refresh.png';
 // import edit from '../images/edit.jpg';
 
-let order =0;
+let order = 0;
 const todoItems = [
-    {
+  {
     text: 'Practice on Vanilla JavaScript',
     completed: false,
-    index: order
-    },
-    {
-        text: 'Practice on React',
-        completed: false,
-        index: order+=1
-    },
-    {
-        text: 'Practice on Ruby on Rails',
-        completed: false,
-        index: order+=1
-    }
+    index: order,
+  },
+  {
+    text: 'Practice on React',
+    completed: false,
+    index: order += 1,
+  },
+  {
+    text: 'Practice on Ruby on Rails',
+    completed: false,
+    index: order += 1,
+  },
 ];
 
 const container = document.querySelector('#container');
@@ -41,11 +40,11 @@ btnDeleteAll.textContent = 'Clear all completed';
 container.appendChild(btnDeleteAll);
 
 const display = () => {
-    ul.innerHTML = '';
-    todoItems.forEach((item) => {
-        const li = document.createElement('li');
-        li.setAttribute('class', 'todo-item');
-        li.innerHTML = `
+  ul.innerHTML = '';
+  todoItems.forEach((item) => {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'todo-item');
+    li.innerHTML = `
         <input type="checkbox" class="checkbox" id="${item.index}">
         <span class="items">${item.text}</span>
         <label for="${item.index}" class="option">
@@ -53,8 +52,8 @@ const display = () => {
         <img class="delete"  src=${delete2} alt="delete"/>
         </label>
         `;
-        ul.appendChild(li);
-    });
-}
+    ul.appendChild(li);
+  });
+};
 
 window.onload = display();
