765fdd50e8ddb6df286988ff5a45988250550642
baqar-abbas <baqar007@hotmail.com>
2024-01-08 03:35:38 +0500

    update live demo link and remove commented pieces of code.

--- src/status.js
+++ src/status.js
@@ -1,10 +1,8 @@
 import { todoItems } from './todo.js';
 
 const updateCompletedStatus = (index, completed) => {
-// const task = todoItems.find((item) => item.index === index+1);
   const task = todoItems.find((item) => item.index === index + 1);
   task.completed = completed;
-  // task.completed = completed;
   localStorage.setItem('todoItems', JSON.stringify(todoItems));
 };
 
